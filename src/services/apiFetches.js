1604e1fc5d30da33cd4ec0d7f489c4b72a146b22
Dan B <dbennington125@gmail.com>
2020-09-22 13:02:34 -0700

    check

--- src/services/apiFetches.js
+++ src/services/apiFetches.js
@@ -6,7 +6,7 @@ export const fetchSearchedPokemon = (search = '') => {
 };
 
 export const getAllPokemon = (page = 1) => {
-  return get(`?page=${page}&perPage=50`);
+  return get(`?page=${page}&perPage=24`);
 };
 
 export const fetchOnePokemon = id => {
@@ -18,9 +18,9 @@ export const fetchAllTypes = () => {
 };
 
 export const fetchByName = (name = '',  page = 1) => {
-  return get(`?pokemon=${name}&page=${page}&perPage=50`);
+  return get(`?pokemon=${name}&page=${page}&perPage=24`);
 };
 
 export const fetchByTypeList = (type, page = 1) => {
-  return get(`?type=${type}&page=${page}&perPage=50`);
+  return get(`?type=${type}&page=${page}&perPage=24`);
 };
